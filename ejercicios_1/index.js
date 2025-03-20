dc78de4ea1953250af6e3635c321f608b239bf3d
lirico <dominguezmatiasadrian@gmail.com>
2025-03-20 18:28:57 -0300

    ejercicios 1

---
+++ ejercicios_1/index.js
@@ -0,0 +1,47 @@
+function mesesDelAño(array) { // ["Enero", "Mayo", "Octumbre", "Noviembre", "Julio", "Marzo"]
+    //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
+    // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
+    //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
+
+    // Resolver con usando "for...of'.
+    // Tu código:
+}
+
+function todosIguales(arreglo) {
+    //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
+    //retornar true, caso contrario retornar false.
+    // Resolver con usando "for...of'.
+    //Escribe tu código aquí:
+}
+
+function mesesDelAño(array) { 
+    //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
+    // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
+    //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
+    // Resolver con usando "for...of'.
+    // Tu código:
+}
+
+function calcularPromedioCalificaciones(estudiante){
+    // Dado un objeto estudiante compuesto por materias con sus calificaciones,
+    // Utiliza un for...in para sumar las calificaciones
+    // Y luego retorna el promedio de las calificaciones.
+    // Resolver con usando "for...in'.
+    // Tu código:
+}
+
+function calcularCantidadProductos(inventario){
+    // Dado un objeto que representa un inventario con productos,
+    // calcular la canitdad de productos que tiene el inventario
+    // Retornar dicha cantidad
+    // Resolver con usando "for...in'.
+    // Tu código:
+}
+
+function filtrarPaisesPorPoblacion(paises){
+    // Dado un objeto con paises, filtrar todos los paises cuya poblacion es mayor a 10
+    // en un array.
+    // Retornar dicho array.
+    // Resolver con usando "for...in'.
+    // Tu código:
+}
