b942f8c41659fff726580dd0daf098cdda5aa9b7
lirico <dominguezmatiasadrian@gmail.com>
2025-03-26 12:32:09 -0300

    closure y recursion

---
+++ clase_marzo_26/closures.js
@@ -0,0 +1,33 @@
+/********* Closures -> Cierres *********/
+
+// function saludar(saludo){
+//     return function(nombre) {
+//         return `${saludo} ${nombre}`
+//     }
+// }
+
+
+// const hola = saludar("Hola")
+// const adios = saludar("Adios")
+// const comoEstas = saludar("Como estas")
+
+// console.log(hola('tomas'))
+// console.log(adios('tomas'))
+// console.log(comoEstas('tomas'))
+
+
+// EL DILEMA DEL CONTADOR
+// function incrementar() {
+//     var contador = 0;
+
+//     return function(){
+//         contador++
+    
+//         console.log(contador)
+//     }
+// }
+
+// incrementar()
+
+// const add = incrementar()
+
---
+++ clase_marzo_26/recursion.js
@@ -0,0 +1,33 @@
+/******** RECURSION *******/
+/* 
+    Es una funcion que se llama a si misma en
+    forma de bucle.
+*/
+
+// function cuentaRegresiva(numero){
+
+//     console.log(numero)
+
+//     let proximoNumero = numero - 1
+
+//     if(proximoNumero >= 0){
+//         cuentaRegresiva(proximoNumero)
+//     }
+// }
+
+// // 3 2 1 0
+
+// cuentaRegresiva(3)
+
+
+// SECUENCIA DE FIBONACCI
+// f(n) = f(n-2) + f(n-1) donde n >= 2;
+// 0, 1, 1, 2, 3, 5, 8, 13...
+
+function fibonacci(n){
+    if (n < 2) {
+        return n;
+    };
+
+    return fibonacci(n - 2) + fibonacci(n - 1);
+}
