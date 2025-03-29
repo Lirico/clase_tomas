a8c78336f5d660c157f8700234770b7c1520ec86
lirico <dominguezmatiasadrian@gmail.com>
2025-03-29 11:25:35 -0300

    ejercicios recursion

---
+++ ejercicios_2/index.js
@@ -0,0 +1,22 @@
+
+function sumaHastaN(n) {    
+    // Escribe una función recursiva que sume todos los números desde 1 hasta n.
+
+}
+
+function factorial(n) {
+    // Escribe una función recursiva para calcular el factorial de un número n.
+
+}
+
+function contarDigitos(numero) {
+    // Escribe una función recursiva que cuente la cantidad de dígitos en un número entero.
+}
+
+function invertirCadena(cadena) {
+    // Escribe una función recursiva que invierta una cadena de texto.
+}
+
+function potencia(base, exponente) {
+    // Escribe una función recursiva que calcule la potencia de un número base elevado a un exponente exponente.
+}
