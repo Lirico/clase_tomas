c3b274713fd18aaa9cf5b9b8a193921a67b958c5
lirico <dominguezmatiasadrian@gmail.com>
2025-04-06 15:34:46 -0300

    mas ejercicios

--- ejercicios_2/index.js
+++ ejercicios_2/index.js
@@ -1,20 +1,24 @@
 
 function sumaHastaN(n) {    
     // Escribe una función recursiva que sume todos los números desde 1 hasta n.
-
 }
 
 function factorial(n) {
     // Escribe una función recursiva para calcular el factorial de un número n.
-
 }
 
 function contarDigitos(numero) {
     // Escribe una función recursiva que cuente la cantidad de dígitos en un número entero.
+    if(numero < 10){
+        return 1;
+    }
+
+    return 1 + contarDigitos(numero / 10 )
 }
 
 function invertirCadena(cadena) {
     // Escribe una función recursiva que invierta una cadena de texto.
+    
 }
 
 function potencia(base, exponente) {
---
+++ ejercicios_3/index.js
@@ -0,0 +1,32 @@
+/************* RECURSION **********/
+
+// Ejercicio 1 - Escribe una función recursiva que reciba un arreglo de números 
+// y devuelva la suma de todos sus elementos.
+function sumaArreglo(arr) {
+
+}
+
+
+// Ejercicio 2 - Escribe una función recursiva que determine si un elemento 
+// específico existe dentro de un arreglo.
+function buscarElemento(arr, elemento) {
+
+}
+
+// Ejercicio 3 - Escribe una función recursiva que encuentre el número más grande en un arreglo de números.
+function encontrarMaximo(arr) {
+
+}
+
+/************* CLOSURES **********/
+
+// Ejercicio 1 - Crea una función que actúe como un contador. Cada vez que se llame al contador, 
+// debe incrementar y devolver el valor actual.
+function createCounter() {
+
+}
+
+// Ejercicio 2 - Crea una función que permita generar saludos personalizados para diferentes nombres.
+function createGreeting(greeting) {
+
+}
