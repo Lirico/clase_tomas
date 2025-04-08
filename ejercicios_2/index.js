82491fac8d4b7a3cf2c6282ef7c9e722e2449b83
Tomas Martinez <TotoXM40@gmai.com>
2025-04-08 17:07:21 -0300

    mas ejercicios

--- ejercicios_2/index.js
+++ ejercicios_2/index.js
@@ -1,26 +1,74 @@
-
+console.log("-------------- sumaHastaN(n) --------------")
 function sumaHastaN(n) {    
     // Escribe una función recursiva que sume todos los números desde 1 hasta n.
-}
 
-function factorial(n) {
-    // Escribe una función recursiva para calcular el factorial de un número n.
-}
-
-function contarDigitos(numero) {
-    // Escribe una función recursiva que cuente la cantidad de dígitos en un número entero.
-    if(numero < 10){
+    if (n === 1) {
         return 1;
     }
 
-    return 1 + contarDigitos(numero / 10 )
+    return n + sumaHastaN(n - 1); 
 }
+
+console.log(sumaHastaN(2))
+
+console.log("-------------- factorial(n) --------------")
+
+
+function factorial(n) {
+    // Escribe una función recursiva para calcular el factorial de un número n.
+
+    if(n === 0 || n === 1){
+        return 1
+    }
+
+    return n * factorial(n - 1)
+}
+
+console.log(factorial(3))
+
+console.log("-------------- contarDigitos(numero) --------------")
+
+
+function contarDigitos(numero) {
+    // Escribe una función recursiva que cuente la cantidad de dígitos en un número entero.
+
+    if(numero < 10){
+        return 1
+    }
+
+    return 1 + contarDigitos(Math.floor(numero / 10)) 
+}
+
+console.log(contarDigitos(9))
+
+console.log("-------------- invertirCadena(cadena) --------------")
+
 
 function invertirCadena(cadena) {
     // Escribe una función recursiva que invierta una cadena de texto.
-    
+
+    if(cadena === ''){
+        return ''
+    }
+
+    return invertirCadena(cadena.slice(1)) + cadena[0]
+
 }
+
+console.log(invertirCadena('hola'))
+
+console.log("-------------- potencia(base, exponente) --------------")
+
 
 function potencia(base, exponente) {
     // Escribe una función recursiva que calcule la potencia de un número base elevado a un exponente exponente.
+
+    if(exponente === 0){
+        return 1
+    }
+
+    return base * potencia(base, exponente - 1)
+
 }
+
+console.log(potencia(2,3))
--- ejercicios_3/index.js
+++ ejercicios_3/index.js
@@ -4,29 +4,77 @@
 // y devuelva la suma de todos sus elementos.
 function sumaArreglo(arr) {
 
+    if(arr === ''){
+        return 0        // si el arreglo esta vacio, retorna 0
+    }
+
+    /* return arr[0] + sumaArreglo(arr.slice(1)) */   // obtengo el primer elemento y lo sumo con los demas
+
 }
 
+console.log(sumaArreglo([]))
 
 // Ejercicio 2 - Escribe una función recursiva que determine si un elemento 
 // específico existe dentro de un arreglo.
 function buscarElemento(arr, elemento) {
 
+    if(arr === ''){
+        return false    // Si el arreglo esta vacio, retorna false
+    }
+
+    if(elemento === arr[0]){
+        return true                          // si el elemento es igual al primer elemento del arreglo devuelve true
+    }
+
+    return buscarElemento(arr.slice(0))      // si no es igual al primer elemento del arreglo lo sacamos y seguimos con el siguiente elemento
+
 }
+
+console.log(buscarElemento())
 
 // Ejercicio 3 - Escribe una función recursiva que encuentre el número más grande en un arreglo de números.
 function encontrarMaximo(arr) {
 
+    if(arr === ''){
+        return 0
+    }
+
+    /* if(arr[0] > arr.slice(1)){
+        return arr
+    } */
+
+    return arr[0] > encontrarMaximo(arr.slice(1))
+
 }
+
+console.log(encontrarMaximo([23,42,12]))
 
 /************* CLOSURES **********/
 
 // Ejercicio 1 - Crea una función que actúe como un contador. Cada vez que se llame al contador, 
 // debe incrementar y devolver el valor actual.
 function createCounter() {
+    let contador = 0
 
+    return function(){
+        contador++
+
+        console.log(contador)
+    }
 }
+
+createCounter()
 
 // Ejercicio 2 - Crea una función que permita generar saludos personalizados para diferentes nombres.
 function createGreeting(greeting) {
 
+    return function(nombre){
+        console.log(`${greeting} ${nombre}`)
+    }
 }
+
+const hola = saludar('hola')
+const chau = saludar('chau')
+
+console.log(hola('Pedro'))
+console.log(chau('Pedro'))
