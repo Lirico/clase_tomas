e768999ad36571c223d881d4ff5e8b63d5a92197
lirico <dominguezmatiasadrian@gmail.com>
2025-04-14 11:24:15 -0300

    ejercicios 4

---
+++ ejercicios_4/index.js
@@ -0,0 +1,37 @@
+/*********** RECURSION  ***********/
+
+
+// Ejercicio 1 - Escribe una función recursiva que determine si una cadena es un palíndromo 
+// (se lee igual de izquierda a derecha que de derecha a izquierda).
+function esPalindromo(cadena) {
+
+}
+
+// Ejercicio 2 - Escribe una función recursiva que cuente cuántas vocales hay en una cadena.
+function contarVocales(cadena) {
+
+}
+
+/*********** CLOSURES  ***********/
+// Ejercicio 1 - Crea una función que permita agregar frases a un banco y luego obtener todas 
+// las frases almacenadas.
+function createPhraseBank() {
+
+}
+
+// Ejercicio 2 - Crea una función que permita generar usuarios con contraseñas y verificar si 
+// la contraseña ingresada es correcta.
+function createUser(username, password) {
+
+}
+
+/*********** FUNCIONES DE ORDEN SUPERIOR  ***********/
+// Ejercicio 1 - Crea una función aplicarDescuento que reciba una lista de precios y una función 
+// que calcule el descuento. La función de descuento debe recibir un precio y devolver el precio 
+// con el descuento aplicado.
+
+
+// Ejercicio 2 - Crear una replica de la funcionalidad del metodo "find" como funcion independiente.
+function find(arreglo, condicion){
+
+}
