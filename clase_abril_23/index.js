523abca94aa6116a4286523445807f91c313d8e8
lirico <dominguezmatiasadrian@gmail.com>
2025-04-23 12:49:53 -0300

    clases intro

---
+++ clase_abril_23/index.js
@@ -0,0 +1,26 @@
+
+
+// Programacion Funcional -> Uso de funciones independientes
+
+// Programacion Orientada a objeto -> Todo pero todo sucede en objetos -> Objetos comunicandose con objetos
+
+// Programacion estructurada
+
+// Basado en prototipos
+// Funcion constructora -> Funcion plantilla -> Genera objetos literales
+
+// Basados en clases
+// Clases -> Objeto plantilla -> Genera objeto literales
+
+// let cadena = new String("Hola")
+
+// let numero = new Number(32)
+
+// let booleano = new Boolean(true)
+
+// let objeto = {}
+
+// console.log(cadena)
+// console.log(numero)
+// console.log(booleano)
+// console.log(objeto)
