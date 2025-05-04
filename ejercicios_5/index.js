a3affbe06d0646c1104866e274a61a723c24a4cf
lirico <dominguezmatiasadrian@gmail.com>
2025-05-04 12:54:20 -0300

    tarea 5

---
+++ ejercicios_5/index.js
@@ -0,0 +1,73 @@
+// Ejercicio 1 (Recursion)
+// Crea una función recursiva que sume solo los números pares de un arreglo.
+function sumaPares(arr) {
+
+}
+
+// Ejercicio 2 (Recursion)
+// Crea una función recursiva que reemplace todas las apariciones de un carácter por otro en una cadena.
+function reemplazarCaracter(cadena, original, nuevo) {
+
+}
+
+// Ejercicio 3 (Closures)
+// Crea una función que permita agregar tareas a una lista y obtener la lista completa, pero sin exponer la lista directamente.
+function createTodoList() {
+
+}
+
+// Ejercicio 4 (Funciones de orden superior)
+// Crea una función llamada componer que reciba dos funciones f y g, y devuelva una nueva función
+//  que, al ser llamada con un valor x, devuelva el resultado de aplicar primero g a x y luego f al resultado.
+function componer(f, g) {
+
+}
+
+// Ejercicio 5 (Funciones de orden superior)
+// Crea una función llamada repetir que reciba una función fn y un número n, y devuelva una nueva función 
+// que, al ser llamada con un valor x, aplique fn a x un total de n veces.
+function repetir(fn, n) {
+
+}
+
+// Ejercicio 6
+/* 
+    Crear clases Persona y Estudiante con herencia
+    Objetivo
+    Crear una clase base Persona con propiedades y métodos básicos.
+
+    Crear una clase derivada Estudiante que herede de Persona y agregue propiedades y métodos propios.
+
+    Practicar el uso de constructor, super(), y métodos personalizados.
+
+    Especificaciones
+    Clase Persona:
+
+    Propiedades:
+
+    nombre (string)
+
+    edad (número)
+
+    Método:
+
+    saludar(): imprime "Hola, mi nombre es [nombre] y tengo [edad] años."
+
+    Clase Estudiante (hereda de Persona):
+
+    Propiedades adicionales:
+
+    carrera (string)
+
+    notas (arreglo de números)
+
+    Métodos adicionales:
+
+    promedio(): calcula y retorna el promedio de las notas.
+
+    Sobrescribe saludar() para que imprima:
+    "Hola, soy [nombre], estudio [carrera] y tengo [edad] años."
+*/
+class Persona {
+
+}
