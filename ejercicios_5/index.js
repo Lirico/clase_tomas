959cbfc7802ae16129513314552b750a632dd4d5
Tomas Martinez <TotoXM40@gmai.com>
2025-05-07 15:40:05 -0300

    tarea 5

--- ejercicios_5/index.js
+++ ejercicios_5/index.js
@@ -2,19 +2,52 @@
 // Crea una función recursiva que sume solo los números pares de un arreglo.
 function sumaPares(arr) {
 
+    if(arr.lenght === 0){
+        return 0
+    }
+
+    const primerElemento = arr[0]
+
+    const resto = arr.slice(1)
+
+    if(primerElemento % 2 === 0){
+        return primerElemento + sumaPares(resto)
+    }
+    else{
+        return sumaPares(resto)
+    }
 }
+
+
 
 // Ejercicio 2 (Recursion)
 // Crea una función recursiva que reemplace todas las apariciones de un carácter por otro en una cadena.
 function reemplazarCaracter(cadena, original, nuevo) {
+
+    if(cadena.lenght === 0){
+        return ''
+    }
+
+
 
 }
 
 // Ejercicio 3 (Closures)
 // Crea una función que permita agregar tareas a una lista y obtener la lista completa, pero sin exponer la lista directamente.
 function createTodoList() {
+    let tareas = []
 
+    return function(tarea){
+        if(tarea){
+            tareas.push(tarea)
+        }
+        else{
+            return frases
+        }
+    }
 }
+
+const lista = createTodoList()
 
 // Ejercicio 4 (Funciones de orden superior)
 // Crea una función llamada componer que reciba dos funciones f y g, y devuelva una nueva función
@@ -69,5 +102,32 @@ function repetir(fn, n) {
     "Hola, soy [nombre], estudio [carrera] y tengo [edad] años."
 */
 class Persona {
+    constructor(nombre, edad){
+        this.nombre = nombre
+        this.edad = edad
+    }
 
+    saludar(){
+        return `Hola, mi nombre es ${nombre} y tengo ${edad} años.`
+    }
+}
+
+class Estudiante extends Persona {
+    constructor(nombre, edad, carrera, notas){
+        super(nombre,edad)
+        this.carrera = carrera
+        this.notas = notas
+    }
+
+    promedio(){
+        let suma = 0
+        for(i = 0; i < this.notas.lenght; i++){
+            suma += notas[i]
+        }
+        return suma / this.notas.lenght
+    }
+
+    saludar(){
+        return `Hola, soy ${nombre}, estudio ${carrera} y tengo ${edad} años.`
+    }
 }
