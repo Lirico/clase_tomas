a4de2dfa3aaba26dd48146bc859dc795cb2f7937
lirico <dominguezmatiasadrian@gmail.com>
2025-05-08 17:18:48 -0300

    Ejercicios resueltos tarea 5

--- ejercicios_5/index.js
+++ ejercicios_5/index.js
@@ -2,7 +2,7 @@
 // Crea una función recursiva que sume solo los números pares de un arreglo.
 function sumaPares(arr) {
 
-    if(arr.lenght === 0){
+    if(arr.length === 0){
         return 0
     }
 
@@ -10,27 +10,28 @@ function sumaPares(arr) {
 
     const resto = arr.slice(1)
 
-    if(primerElemento % 2 === 0){
-        return primerElemento + sumaPares(resto)
-    }
-    else{
-        return sumaPares(resto)
-    }
-}
+    const suma = primerElemento % 2 === 0 ? primerElemento : 0
 
+    return suma + sumaPares(resto)
+}
 
 
 // Ejercicio 2 (Recursion)
 // Crea una función recursiva que reemplace todas las apariciones de un carácter por otro en una cadena.
 function reemplazarCaracter(cadena, original, nuevo) {
 
-    if(cadena.lenght === 0){
-        return ''
-    }
+    if(cadena.length === 0) return ''
 
+    const cadenaRecortada = cadena.slice(1)
 
+    const primerElemento = cadena[0]
+    
+    const caracter = primerElemento === original ? nuevo : primerElemento
 
+    return caracter + reemplazarCaracter(cadenaRecortada, original, nuevo)
 }
+
+
 
 // Ejercicio 3 (Closures)
 // Crea una función que permita agregar tareas a una lista y obtener la lista completa, pero sin exponer la lista directamente.
@@ -41,27 +42,35 @@ function createTodoList() {
         if(tarea){
             tareas.push(tarea)
         }
-        else{
-            return frases
-        }
+        return tareas
     }
 }
-
-const lista = createTodoList()
 
 // Ejercicio 4 (Funciones de orden superior)
 // Crea una función llamada componer que reciba dos funciones f y g, y devuelva una nueva función
 //  que, al ser llamada con un valor x, devuelva el resultado de aplicar primero g a x y luego f al resultado.
-function componer(f, g) {
 
+function componer(f, g) {
+    return function(x) { 
+        return f(g(x))
+    }
 }
 
 // Ejercicio 5 (Funciones de orden superior)
 // Crea una función llamada repetir que reciba una función fn y un número n, y devuelva una nueva función 
 // que, al ser llamada con un valor x, aplique fn a x un total de n veces.
 function repetir(fn, n) {
+    return function(x) {
+        let resultado = x;
 
+        for(let i = 0; i < n; i++){
+            resultado = fn(x) 
+        }
+        return resultado;
+    }
 }
+
+// Tenemos que usar un for para aplicar fn a x .... tantas veces (n veces).
 
 // Ejercicio 6
 /* 
@@ -121,10 +130,10 @@ class Estudiante extends Persona {
 
     promedio(){
         let suma = 0
-        for(i = 0; i < this.notas.lenght; i++){
+        for(i = 0; i < this.notas.length; i++){
             suma += notas[i]
         }
-        return suma / this.notas.lenght
+        return suma / this.notas.length
     }
 
     saludar(){
