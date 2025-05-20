b8cda8c55cbdf34b55cd75ce67fa14602cfed330
Tomas Martinez <TotoXM40@gmai.com>
2025-05-20 17:46:27 -0300

    ejercicios 7

--- ejercicios_7/index.js
+++ ejercicios_7/index.js
@@ -1,30 +1,78 @@
 // Ejercicio 1 (Recursion) - Escribe una función recursiva que cuente cuántas veces
 // aparece un dígito específico dentro de un número entero.
 function contarDigito(numero, digito) {
-  // Tu codigo:
+    if (numero.length == 0) {
+        return 0
+    }
+
+    const newArray = numero += []
+
+    if (newArray[0].includes(digito)) {
+        return 1 + contarDigito(numero.slice(1), digito)
+    }
+
+    return contarDigito(numero.slice(1), digito)
 }
+
+console.log(contarDigito(4314141141, 4))
 
 // Ejercicio 2 (Funciones de Orden Superior) - Crea una función llamada crearMultiplicador
 // que reciba un número n y devuelva una nueva función que reciba otro número y lo multiplique por n.
 function crearMultiplicador(n) {
-  // Tu codigo:
+    return function (x) {
+        return x * n
+    }
 }
+
 
 // Ejercicio 3 (Funciones de Orden Superior) - Crea una función llamada contarSi que reciba un arreglo
 // y una función de condición, y devuelva cuántos elementos del arreglo cumplen esa condición.
 function contarSi(arreglo, fnCondicion) {
-  // Tu codigo:
+    let contador = 0
+    for(i = 0; i < arreglo.length; i++){
+        
+    }
+    return fnCondicion(arreglo)
 }
+
+function par(arreglo){
+    return arreglo % 2 === 0
+
+}
+
+
+console.log(contarSi([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], par))
 
 // Ejercicio 4 (Funciones de Orden Superior) - Crea una función llamada aplicarFunciones que reciba un
 // arreglo de funciones y un valor inicial, y aplique cada función al resultado acumulado, devolviendo
 // el resultado final.
+
+const funciones = [suma, resta, multi]
+
 function aplicarFunciones(funciones, valorInicial) {
-  // Tu codigo:
+    let resultado = valorInicial
+    for(let i = 0; i < funciones.length; i++){
+        resultado = funciones[i](resultado) //llama a la funcion en la posicion de i y aplica el resultado actual
+    }
+    return resultado //una vez que termina el for, muestra el resultado de todas las funciones 
 }
 
+function suma(n){
+    return n + 5
+}
+
+function resta(n){
+    return n - 2
+}
+
+function multi(n){
+    return n * 10
+}
+
+console.log(aplicarFunciones(funciones, 5))
+
 // Ejercicio 5 (Clases)
-/* 
+/*
     Implementar la clase Universidad.
 
     Especificaciones
@@ -77,3 +125,73 @@ function aplicarFunciones(funciones, valorInicial) {
                 - Más de 5 ambientes: 35 % de impuesto
         - info(): Debe devolver una cadena de texto con la calle, el numero y la localidad.
 */
+
+class Universidad{
+    constructor(nombre, cantidadAlumnos, carreras){
+        this.nombre = nombre
+        this.cantidadAlumnos = cantidadAlumnos
+        this.carreras = carreras
+    }
+
+    dameNombre(){
+        return this.nombre
+    }
+
+    info(){
+        return `Nombre: ${this.nombre}, Cantidad de Alumnos: ${this.cantidadAlumnos}, Carreras: ${this.carreras}`
+    }
+    
+}
+
+const harvard = new Universidad('Harvard', 20000, ['Derecho', 'Economia', 'Ciencias Politicas'])
+console.log(harvard.info())
+
+class Propiedad{
+    constructor(calle, numero, localidad, añoDeConstruccion, cantidadDeAmbientes){
+        this.calle = calle
+        this.numero = numero
+        this.localidad = localidad
+        this.añoDeConstruccion = añoDeConstruccion
+        this.cantidadDeAmbientes = cantidadDeAmbientes
+    }
+
+    mismaLocalidad(){
+
+    }
+
+    mismaCalle(){
+
+    }
+
+    mayorNumeración(){
+
+    }
+
+    calculaImpuestoARBA(){
+        if(this.añoDeConstruccion >= 1871 && this.añoDeConstruccion <= 1949){
+            if(this.cantidadDeAmbientes >= 1 && this.cantidadDeAmbientes <= 3){
+                return '5% de impuesto'
+            }
+            else if(this.cantidadDeAmbientes >= 4 && this.cantidadDeAmbientes <= 6){
+                return '10% de impuesto'
+            }
+            else{
+                return '25% de impuesto'
+            }
+        }
+        else if(this.añoDeConstruccion >= 1950 && this.añoDeConstruccion <= 2025){
+            if(this.cantidadDeAmbientes >= 1 && this.cantidadDeAmbientes <= 5){
+                return '5% de impuesto'
+            }
+            else{
+                return '35% de impuesto'
+            }
+        }
+    }
+
+    info(){
+        return `Calle: ${this.calle}, Numero: ${this.numero}, Localidad: ${this.localidad}`
+    }
+}
+
+const propiedad = new Propiedad()
